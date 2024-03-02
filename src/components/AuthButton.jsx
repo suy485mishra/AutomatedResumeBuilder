2844e9bb4db8fbb64a94cdfa16a5f20cca9c93e1
Suyash Mishra <121488134+suy485mishra@users.noreply.github.com>
2024-03-02 15:15:50 +0530

    d2

--- src/components/AuthButton.jsx
+++ src/components/AuthButton.jsx
@@ -1,49 +1,68 @@
-import React from 'react'
-import {FaChevronRight} from 'react-icons/fa6'
+import React from "react";
+import { FaChevronRight } from "react-icons/fa6";
 
 //importing different auths
-import {GoogleAuthProvider,GithubAuthProvider, signInWithRedirect} from 'firebase/auth'
-import { auth } from '../config/firebase.config';
+import {
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithRedirect,
+} from "firebase/auth";
+import { auth } from "../config/firebase.config";
 
+const AuthButton = ({ Icon, label, provider }) => {
+  const googleAuthProvider = new GoogleAuthProvider();
+  const githAuthProvider = new GithubAuthProvider();
 
-const AuthButton = ({Icon, label,provider}) => {
+  const handleClick = async () => {
+    switch (provider) {
+      case "GoogleAuthProvider":
+        await signInWithRedirect(auth, googleAuthProvider)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(`${err.Message}`);
+          });
+        break;
 
-  const googleAuthProvider=new GoogleAuthProvider();
-  const githAuthProvider=new GithubAuthProvider();
+      case "GithubAuthProvider":
+        await signInWithRedirect(auth, githAuthProvider)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(`${err.Message}`);
+          });
+        break;
 
-const handleClick=async()=>{
- 
-  switch(provider){
-    case "GoogleAuthProvider":
-    await signInWithRedirect(auth,googleAuthProvider).then((res)=>{console.log(res)}).catch((err)=>{console.log(`${err.Message}`)})
-      break;
-  
-
-    case "GithubAuthProvider":
-      await signInWithRedirect(auth,githAuthProvider).then((res)=>{console.log(res)}).catch((err)=>{console.log(`${err.Message}`)})
-      break;
-  
-  default:
-    await signInWithRedirect(auth,googleAuthProvider).then((res)=>{console.log(res)}).catch((err)=>{console.log(`${err.Message}`)})
-    break;
-}
-}
+      default:
+        await signInWithRedirect(auth, googleAuthProvider)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(`${err.Message}`);
+          });
+        break;
+    }
+  };
 
   return (
-        <div onClick={handleClick} 
-        className=' w-full px-2 py-3 rounded-md border-2 border-blue-700 
+    <div
+      onClick={handleClick}
+      className=" w-full px-2 py-3 rounded-md border-2 border-blue-700 
         flex items-center justify-between cursor-pointer group hover:bg-blue-800 
-        active:scale-75 duration-150 hover: shadow-md'>
+        active:scale-75 duration-150 hover: shadow-md"
+    >
+      <Icon className="text-txtPrimary text-xl group-hover:text-white  " />
 
-        <Icon className='text-txtPrimary text-xl group-hover:text-white  '/>
+      <p className="text-txtPrimary text-xl group-hover:text-white  ">
+        {label}
+      </p>
 
-<p className='text-txtPrimary text-xl group-hover:text-white  '>{label}</p>
-
-<FaChevronRight className='text-txtPrimary text-lg group-hover:text-white'/>
-  
-  
+      <FaChevronRight className="text-txtPrimary text-lg group-hover:text-white" />
     </div>
-  )
-}
+  );
+};
 
-export default AuthButton
+export default AuthButton;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,27 +1,34 @@
-import React, { Suspense } from 'react'
-import { Route,Routes } from 'react-router-dom'
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
 //home &auth route
-import { HomeScreen,Auth } from '../pages'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import {ReactQueryDevtools} from 'react-query/devtools'
-import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import { HomeScreen, Auth, CreateTemplate ,UserProfile,CreateResume,TemplateDesignPinDetails} from "../pages";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Header } from "../components";
 
 const App = () => {
-  const queryClient=new QueryClient();
+  const queryClient = new QueryClient();
 
   return (
-     <QueryClientProvider client={queryClient}>
-   <Suspense fallback={<div>Loading...</div>}>
-<Routes>
-    <Route path='/' element={<HomeScreen/>}/>
-    <Route path='/auth' element={<Auth/>}/>
-</Routes>
-  </Suspense>
-  <ReactQueryDevtools initialIsOpen={false}/>
-  <ToastContainer position='top-right' theme='dark' />
- </QueryClientProvider>
- 
- )}
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/template/create" element={<><Header /> <CreateTemplate/></>} />
+          <Route path="/profile/:uid" element={<><Header /> <UserProfile/></>}/>
+          <Route path="/resume/*" element={<><Header /> <CreateResume/></>}/>
+          <Route path="/resumeDetail/:templateID" element={<><Header /> <TemplateDesignPinDetails/></>}/>
 
-export default App
+           
+        </Routes>
+      </Suspense>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ToastContainer position="top-right" theme="dark" />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
