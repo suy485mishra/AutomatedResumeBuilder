47d098f3633206e17fd26a745b69472139074730
Suyash Mishra <121488134+suy485mishra@users.noreply.github.com>
2024-03-11 09:53:29 +0530

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -167,7 +167,7 @@ const Header = () => {
 
 
         
-      
+            
     </header>
   );
 };
--- src/translate/translate.js
+++ src/translate/translate.js
@@ -5,12 +5,21 @@ const GoogleTranslate = () => {
     const script = document.createElement('script');
     script.src = "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
     script.async = true;
+
+    // Add event listener to handle script loading errors
+    script.onerror = (error) => {
+      console.error('Error loading Google Translate script:', error);
+    };
+
+    // Append script to the document body
     document.body.appendChild(script);
 
+    // Define Google Translate initialization function
     window.googleTranslateElementInit = () => {
       new window.google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
     };
 
+    // Clean up function
     return () => {
       document.body.removeChild(script);
       delete window.googleTranslateElementInit;
